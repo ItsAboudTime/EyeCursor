411c1e54293b78d34394531ca5a84e187a704a4f
ItsAboudTime <a.h.abusaad@gmail.com>
2026-03-29 22:46:58 +0300

    Fix double click bug on macOS

--- cursor/macos.py
+++ cursor/macos.py
@@ -1,14 +1,17 @@
+import time
 from typing import Tuple
 from cursor.base import Cursor
 
 from Quartz import (
     CGEventCreateMouseEvent,
     CGEventPost,
+    CGEventSetIntegerValueField,
     kCGEventMouseMoved,
     kCGEventLeftMouseDown,
     kCGEventLeftMouseUp,
     kCGEventRightMouseDown,
     kCGEventRightMouseUp,
+    kCGMouseEventClickState,
     CGDisplayBounds,
     CGMainDisplayID,
     CGEventCreate,
@@ -17,8 +20,23 @@ from Quartz import (
 )
 
 
-# Currently only supports single display setups.
 class MacOSCursor(Cursor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._double_click_max_interval = 0.45
+        self._left_click_count = 0
+        self._right_click_count = 0
+        self._left_last_up_time = 0.0
+        self._right_last_up_time = 0.0
+        self._left_down_click_count = 1
+        self._right_down_click_count = 1
+
+    def _next_click_count(self, last_up_time: float, current_click_count: int) -> int:
+        now = time.perf_counter()
+        if now - last_up_time <= self._double_click_max_interval:
+            return min(3, current_click_count + 1)
+        return 1
+
     def get_pos(self) -> Tuple[int, int]:
         event = CGEventCreate(None)
         location = CGEventGetLocation(event)
@@ -43,24 +61,40 @@ class MacOSCursor(Cursor):
         self.left_up()
 
     def left_down(self) -> None:
+        self._left_down_click_count = self._next_click_count(
+            self._left_last_up_time,
+            self._left_click_count,
+        )
         event_down = CGEventCreateMouseEvent(None, kCGEventLeftMouseDown, self.get_pos(), 0)
+        CGEventSetIntegerValueField(event_down, kCGMouseEventClickState, self._left_down_click_count)
         CGEventPost(0, event_down)
 
     def left_up(self) -> None:
         event_up = CGEventCreateMouseEvent(None, kCGEventLeftMouseUp, self.get_pos(), 0)
+        CGEventSetIntegerValueField(event_up, kCGMouseEventClickState, self._left_down_click_count)
         CGEventPost(0, event_up)
+        self._left_click_count = self._left_down_click_count
+        self._left_last_up_time = time.perf_counter()
 
     def right_click(self) -> None:
         self.right_down()
         self.right_up()
 
     def right_down(self) -> None:
+        self._right_down_click_count = self._next_click_count(
+            self._right_last_up_time,
+            self._right_click_count,
+        )
         event_down = CGEventCreateMouseEvent(None, kCGEventRightMouseDown, self.get_pos(), 0)
+        CGEventSetIntegerValueField(event_down, kCGMouseEventClickState, self._right_down_click_count)
         CGEventPost(0, event_down)
 
     def right_up(self) -> None:
         event_up = CGEventCreateMouseEvent(None, kCGEventRightMouseUp, self.get_pos(), 0)
+        CGEventSetIntegerValueField(event_up, kCGMouseEventClickState, self._right_down_click_count)
         CGEventPost(0, event_up)
+        self._right_click_count = self._right_down_click_count
+        self._right_last_up_time = time.perf_counter()
 
     def scroll(self, delta: int) -> None:
         event = CGEventCreateScrollWheelEvent(None, 0, 1, delta)
